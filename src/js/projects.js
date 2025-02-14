65487403c667f68970cb9f2f6b5ddd567fba1965
TaniaFed <ktv.poshta@gmail.com>
2025-02-14 10:42:16 -0500

    Merge branch 'fixing-bugs'

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // / Import Swiper and modules
 // import Swiper from 'swiper';
 // import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
@@ -49,6 +50,29 @@ const projectList = document.querySelector('.projects-list');
 
 const createProjectEl = obj =>{
     return`
+=======
+import projectImgOne from '../img/imgJPEG/projectImg/projectImg1.jpg';
+import projectImgTwo from  '../img/imgJPEG/projectImg/projectImg2.jpg';
+import projectImgThree from '../img/imgJPEG/projectImg/projectImg3.jpg';
+import pathBtn from '../img/imgSVG/sprite.svg';
+const projectsArr = [
+  {
+    projectName: 'Programming Across Borders: Ideas, Technologies, Innovations',
+    img: projectImgOne,
+  },
+  {
+    projectName: `Programming Across Borders: Ideas, Technologies, Innovations`,
+    img: projectImgTwo,
+  },
+  {
+    projectName: 'Programming Across Borders: Ideas, Technologies, Innovations',
+    img: projectImgThree,
+  },
+];
+const projectList = document.querySelector('.projects-list');
+const createProjectEl = obj => {
+  return `
+>>>>>>> Stashed changes
     <div class='project-link-container'>
         <div class='proj-link-container'>
             <ul class='skill-proj-list'>
@@ -57,13 +81,18 @@ const createProjectEl = obj =>{
                 <li class = 'skill-proj-link'>#node.js</li>
                 <li class = 'skill-proj-link'>#git</li>
             </ul>
+<<<<<<< Updated upstream
             <p class='project-name-p'>${obj.projectName}</p>
+=======
+          <p class='project-name-p'>${obj.projectName}</p>
+>>>>>>> Stashed changes
             <button class='send-button-proj'>Send project</button>
         </div>
         <div class='project-image-link-cont'>
             <img class = 'project-image'src="${obj.img}" alt="${obj.projectName}">
         </div>
     </div>
+<<<<<<< Updated upstream
     `
 };
 
@@ -73,3 +102,18 @@ const addProjctTemplate = template => projectList.insertAdjacentHTML('beforeend'
 
 
 addProjctTemplate(createProjectTemplate(projectsArr));
+=======
+    `;
+};
+const createProjectTemplate = arr =>
+  arr.map(el => createProjectEl(el)).join('');
+const addProjectTemplate = template =>
+  projectList.insertAdjacentHTML('beforeend', template);
+addProjectTemplate(createProjectTemplate(projectsArr));
+
+const rightButnnProj = document.querySelector('.right-arrow-prjct');
+const leftButnnProj = document.querySelector('.left-arrow-prjct');
+
+leftButnnProj.setAttribute('href', `${pathBtn}#arrowLeft`);
+rightButnnProj.setAttribute('href', `${pathBtn}#arrowRight`);
+>>>>>>> Stashed changes
