73be80ae1b943f26e61afe2a09fed9c06e66e1a2
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-02-15 15:31:26 +0100

    Update footer.js

--- src/js/footer.js
+++ src/js/footer.js
@@ -4,25 +4,35 @@ import { addImg } from "./header";
 
 addImg(document.querySelector('.footer-title-fl'), createImg(logoImg,'img-logo'));
 
-
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("formfield");
   const modal = document.getElementById("modal");
   const closeModalBtn = document.querySelector(".close-modal");
+  
+  function toggleMenu() {
+    const body = document.body;
+    
+  if (menuMobCont.classList.contains("menu-div-is-open")) {
+    body.classList.add("no-scroll");
+  } else {
+    body.classList.remove("no-scroll");
+    }  
+}
+
+
+
 
   form.addEventListener("submit", function (event) {
     event.preventDefault(); 
-
-   
+    
     modal.classList.remove("is-hidden");
+  form.reset();
   });
 
-  form.reset();
+  toggleMenu()
   
   closeModalBtn.addEventListener("click", function () {
+    toggleMenu()
     modal.classList.add("is-hidden");
   });
 
@@ -39,7 +49,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
-
 
 
 
